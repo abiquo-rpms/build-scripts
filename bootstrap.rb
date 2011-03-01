d096727dc48c7bb63b38d5bf410760a205842a68
Sergio Rubio <sergio@rubio.name>
2011-03-01 14:34:26 +0100

    * updated readme * add --skip-fetch to bootstrap.rb

--- bootstrap.rb
+++ bootstrap.rb
@@ -39,18 +39,20 @@ end
 
 Dir.mkdir REPOS_BASE if not File.exist?(REPOS_BASE)
 
-repo_number = YAML.load(RestClient.get 'http://github.com/api/v2/yaml/user/show/abiquo-rpms')['user']['public_repo_count'].to_i
-pages = (repo_number/30.0).ceil
+if not ARGV.include?('--skip-fetch')
+  repo_number = YAML.load(RestClient.get 'http://github.com/api/v2/yaml/user/show/abiquo-rpms')['user']['public_repo_count'].to_i
+  pages = (repo_number/30.0).ceil
 
-repos = []
-1.upto(pages) do |page|
-  repos += YAML.load(RestClient.get GH_REPOS_URL + "?page=#{page}")['repositories']
-end
+  repos = []
+  1.upto(pages) do |page|
+    repos += YAML.load(RestClient.get GH_REPOS_URL + "?page=#{page}")['repositories']
+  end
 
-repos = repos.sort { |a,b| a[:name] <=> b[:name] }
-repos.each do |repo|
-  puts "* Fetching #{repo[:name]} repo..."
-  PKGWizard::GitRPM.fetch GH_URL + "/#{repo[:name]}", "#{REPOS_BASE}/#{repo[:name]}", :depth => 1
+  repos = repos.sort { |a,b| a[:name] <=> b[:name] }
+  repos.each do |repo|
+    puts "* Fetching #{repo[:name]} repo..."
+    PKGWizard::GitRPM.fetch GH_URL + "/#{repo[:name]}", "#{REPOS_BASE}/#{repo[:name]}", :depth => 1
+  end
 end
 
 Dir.chdir REPOS_BASE
